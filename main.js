c0d715ba037e578eaf3fd92a1d4d84a78a784827
plj saveli <saveli.pljasunov@gmail.com>
2024-05-15 12:56:21 +0300

    text changes

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ function collisionDetection() {
           b.status = 0;
           score++;
           if (score == brickRowCount * brickColumnCount) {
-            alert("YOU WIN, CONGRATS!");
+            alert("VICTORY!");
             document.location.reload();
           }
         }
@@ -146,7 +146,7 @@ function draw() {
     } else {
       lives--;
       if (!lives) {
-        alert("GAME OVER");
+        alert("DEFEAT!");
         document.location.reload();
       } else {
         x = canvas.width / 2;
